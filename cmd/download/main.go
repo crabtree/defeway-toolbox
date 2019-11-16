271bb1e4b1f23152de9d5d0d05f1564565536a35
crabtree <crabtree@users.noreply.github.com>
2019-11-16 23:36:53 +0100

    Move command logic to downloader package

--- cmd/download/main.go
+++ cmd/download/main.go
@@ -7,6 +7,7 @@ import (
 	"path"
 	"sync"
 
+	"github.com/crabtree/defeway-toolbox/internal/downloader"
 	"github.com/crabtree/defeway-toolbox/pkg/defewayclient"
 )
 
@@ -22,30 +23,42 @@ func main() {
 		fmt.Sprintf("%s:%d", params.Address, params.Port),
 		params.Username,
 		params.Password)
+
 	recMgr := defewayclient.NewRecordingsManager(client)
-	recordings, err := recMgr.Fetch(paramsToRecordingsFetchParams(params))
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	recordingsChan := make(chan defewayclient.RecordingMeta)
+
+	cmd := downloader.NewCmd(*recMgr, recordingsChan)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		err := cmd.FetchRecordings(paramsToRecordingsFetchParams(params))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
 	outDir := path.Join(params.OutputDir, params.Address.String())
 	if err = ensureRecordingsDir(outDir); err != nil {
 		log.Fatal(err)
 	}
 
-	recordingsChan := make(chan defewayclient.RecordingMeta, len(recordings))
-
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-	go addRecordingsToChannel(&wg, recordings, recordingsChan)
-
 	for i := 0; i < params.Concurrent; i++ {
 		wg.Add(1)
-		go downloadRecording(&wg, recordingsChan, outDir, params.Overwrite, recMgr)
+		go func() {
+			defer wg.Done()
+			err := cmd.ProcessRecordings(outDir, params.Overwrite)
+			if err != nil {
+				log.Println(err)
+			}
+		}()
 	}
 
-	log.Printf("Waiting to process the recordings (%d)\n", len(recordings))
+	log.Println("Waiting to process the recordings")
 	wg.Wait()
 }
 
@@ -59,16 +72,6 @@ func paramsToRecordingsFetchParams(params *params) defewayclient.RecordingsFetch
 	}
 }
 
-func addRecordingsToChannel(wg *sync.WaitGroup, recordings []defewayclient.RecordingMeta, recordingsChan chan<- defewayclient.RecordingMeta) {
-	defer wg.Done()
-
-	for _, rec := range recordings {
-		recordingsChan <- rec
-	}
-
-	close(recordingsChan)
-}
-
 func ensureRecordingsDir(dirPath string) error {
 	_, err := os.Stat(dirPath)
 	if os.IsNotExist(err) {
@@ -77,55 +80,4 @@ func ensureRecordingsDir(dirPath string) error {
 	}
 
 	return err
-}
-
-func downloadRecording(
-	wg *sync.WaitGroup,
-	recordingsChan <-chan defewayclient.RecordingMeta,
-	outDir string,
-	overwrite bool,
-	recMgr *defewayclient.RecordingsManager) {
-
-	defer wg.Done()
-
-	for rec := range recordingsChan {
-		dstPath := path.Join(outDir, fmt.Sprintf("%d.flv", rec.RecordingID))
-		exists, err := fileExists(dstPath)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		if exists && !overwrite {
-			log.Printf("File %s already exists\n", dstPath)
-			continue
-		}
-
-		log.Printf("Downloading %d into %s\n", rec.RecordingID, dstPath)
-
-		dst, err := os.Create(dstPath)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		defer dst.Close()
-
-		if err = recMgr.Download(rec, dst); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
-func fileExists(dstPath string) (bool, error) {
-	_, err := os.Stat(dstPath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return false, nil
-		}
-
-		return false, err
-	}
-
-	return true, nil
 }
---
+++ internal/downloader/downloader.go
@@ -0,0 +1,90 @@
+package downloader
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+
+	dc "github.com/crabtree/defeway-toolbox/pkg/defewayclient"
+)
+
+type cmd struct {
+	recMgr  dc.RecordingsManager
+	recChan chan dc.RecordingMeta
+}
+
+func NewCmd(recMgr dc.RecordingsManager, recsChan chan dc.RecordingMeta) *cmd {
+	return &cmd{
+		recMgr:  recMgr,
+		recChan: recsChan,
+	}
+}
+
+func (c *cmd) FetchRecordings(params dc.RecordingsFetchParams) error {
+	defer close(c.recChan)
+
+	recordings, err := c.recMgr.Fetch(params)
+	if err != nil {
+		return err
+	}
+
+	for _, rec := range recordings {
+		c.recChan <- rec
+	}
+
+	return nil
+}
+
+func (c *cmd) ProcessRecordings(outDir string, overwrite bool) error {
+	for rec := range c.recChan {
+		dstPath := path.Join(outDir, fmt.Sprintf("%d.flv", rec.RecordingID))
+		exists, err := fileExists(dstPath)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		if exists && !overwrite {
+			log.Printf("File %s already exists\n", dstPath)
+			continue
+		}
+
+		log.Printf("Downloading %d into %s\n", rec.RecordingID, dstPath)
+
+		err = c.downloadFile(dstPath, rec)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+	}
+
+	return nil
+}
+
+func (c *cmd) downloadFile(dstPath string, recMeta dc.RecordingMeta) error {
+	dst, err := os.Create(dstPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if err = c.recMgr.Download(recMeta, dst); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fileExists(dstPath string) (bool, error) {
+	_, err := os.Stat(dstPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+
+		return false, err
+	}
+
+	return true, nil
+}
