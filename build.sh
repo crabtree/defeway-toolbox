a367db559a9af8f160116dc9daf1105212839c49
Krystian Cieślik <3641496+crabtree@users.noreply.github.com>
2022-03-11 15:12:26 +0100

    Add windows 386 build

--- build.sh
+++ build.sh
@@ -7,9 +7,11 @@ echo "Building ..."
 [ ! -d bin ] && mkdir bin
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/defewaydownload ./cmd/download
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/defewaydownload.exe ./cmd/download
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/defewaydownload-amd64.exe ./cmd/download
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o bin/defewaydownload-x86.exe ./cmd/download
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/defewayscan ./cmd/scan
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/defewayscan.exe ./cmd/scan
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/defewayscan-amd64.exe ./cmd/scan
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o bin/defewayscan-x86.exe ./cmd/scan
 
 echo "... DONE!"
