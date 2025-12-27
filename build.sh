0e81ebc67a213086566f71f53a045fb4b37983f9
crabtree <3641496+crabtree@users.noreply.github.com>
2025-12-27 19:25:15 +0100

    Add apple build (#36)

--- build.sh
+++ build.sh
@@ -6,10 +6,12 @@ go test ./...
 echo "Building ..."
 [ ! -d bin ] && mkdir bin
 
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o bin/defewaydownload-macos ./cmd/download
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/defewaydownload ./cmd/download
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/defewaydownload-amd64.exe ./cmd/download
 CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o bin/defewaydownload-x86.exe ./cmd/download
 
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o bin/defewayscan-macos ./cmd/scan
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/defewayscan ./cmd/scan
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/defewayscan-amd64.exe ./cmd/scan
 CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o bin/defewayscan-x86.exe ./cmd/scan
